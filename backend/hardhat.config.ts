b4d8f14a87eed11b38c524a51f622fdac0c55093
AlexScotte <scottealexandre@gmail.com>
2024-04-04 10:42:53 +0200

    [back] add deployment script

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,39 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const INFURA_ID = process.env.INFURA_ID || "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: `${ETHERSCAN_API_KEY}`,
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_ID}`,
+      // accounts: [`0x${PRIVATE_KEY}`],
+      accounts: {
+        mnemonic: `${process.env.MNEMONIC}`,
+      },
+      chainId: 5,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_ID}`,
+      accounts: {
+        mnemonic: `${process.env.MNEMONIC}`,
+      },
+      chainId: 80001,
+      gasPrice: 80000000000,
+    },
+  },
 };
 
 export default config;
---
+++ backend/scripts/deploy.ts
@@ -0,0 +1,122 @@
+import { ethers } from 'hardhat';
+import hre from 'hardhat';
+import type {
+    BaseContract,
+} from "ethers";
+
+const contractFrontFolder = "../../frontend/contracts";
+const artifactsFolder = "../artifacts/contracts";
+
+
+// Types
+import { SimpleStorage } from '../typechain-types';
+
+async function main() {
+
+    let contract: SimpleStorage;
+
+    try {
+        // TODO: manage signer for other networks
+        const [owner] = await ethers.getSigners();
+        contract = await ethers.deployContract('SimpleStorage', [owner.address]);
+        await contract.waitForDeployment();
+
+        console.log(
+            `SimpleStorage deployed to: ${contract.target} by: ${owner.address}`
+        )
+    }
+    catch (error) {
+        throw new Error(`Error when deploying contract: ${error}`);
+    }
+
+    await saveFrontendFiles("SimpleStorage", contract);
+}
+
+async function saveFrontendFiles(contractName: string, contract: BaseContract) {
+    const fs = require("fs");
+    const path = require("path");
+
+    const frontContractsDir = path.join(__dirname, contractFrontFolder);
+    const frontContractFilePath = path.join(frontContractsDir, `${contractName}.json`);
+
+    console.log(`Frontend contract directory: ${frontContractsDir}`);
+    if (!fs.existsSync(frontContractsDir)) {
+        fs.mkdirSync(frontContractsDir);
+    }
+
+    const backArtifactsDir = path.join(__dirname, artifactsFolder);
+    const backContractArtifactPath = path.join(backArtifactsDir, `${contractName}.sol/${contractName}.json`);
+
+    console.log(`Backend contract artifact path: ${backContractArtifactPath}`);
+
+
+    const networkName = hre.network.name
+    const chainId = hre.network.config.chainId ?? "";
+    if (!chainId)
+        throw new Error("Chain ID not found in hardhat.config.ts file. Please add chainId to the network configuration.");
+
+    let previousArtifact: any;
+    let currentArtifact: any;
+    let abiChanged: boolean = false;
+
+    try {
+        // Read current artifact
+        currentArtifact = JSON.parse(
+            fs.readFileSync(backContractArtifactPath, "utf8")
+        );
+    } catch (err) {
+        throw new Error("Error when reading artifact, verify that you build the contract first and check the contract artifact path");
+    }
+
+    try {
+        // Read previous deployed artifact
+        previousArtifact = JSON.parse(
+            fs.readFileSync(frontContractFilePath, "utf8")
+        );
+
+        // If the contract changed, we remove all previous network informations
+        if (JSON.stringify(currentArtifact.abi) != JSON.stringify(previousArtifact.abi)) {
+            abiChanged = true;
+            console.log("ABI changed, you need to redeploy the contract on all networks");
+        }
+
+
+    } catch (err) {
+        console.error("No previous artifact");
+        previousArtifact = currentArtifact;
+        // previousArtifact = { networks: {} };
+    }
+
+    // Write deployed contract informations in a file
+    console.log(`Write deployed ${contractName} informations in a ${frontContractFilePath}`);
+    if (abiChanged || !previousArtifact.hasOwnProperty("networks")) {
+        previousArtifact.networks = {};
+    }
+
+    // Create chain ID node in networks section
+    if (!previousArtifact.networks.hasOwnProperty(chainId)) {
+        previousArtifact.networks[chainId] = {};
+    }
+
+    previousArtifact.networks[chainId].address = contract.target;
+    previousArtifact.networks[chainId].network = networkName;
+    const deployTransaction = contract.deploymentTransaction();
+    if (deployTransaction) {
+        previousArtifact.networks[chainId].transactionHash =
+            deployTransaction.hash;
+        previousArtifact.networks[chainId].blockNumber =
+            deployTransaction.blockNumber;
+    }
+
+    currentArtifact.networks = previousArtifact.networks;
+
+    fs.writeFileSync(
+        frontContractFilePath,
+        JSON.stringify(currentArtifact, null, 2)
+    );
+};
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
